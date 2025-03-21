5b5ec1c9d71a2cfcdba4ec7b71cc473bf4d9672f
Winstar <james.canoy87@gmail.com>
2025-03-22 01:53:15 +0800

    Algorithm for this program

---
+++ convert_to_titlecase.py
@@ -0,0 +1,9 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing. 
+# Print the input in proper casing.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: Juan Dela Cruz
+
+# 1. Prompt the user to input their fullname.
+# 2. Convert the input string to lowercase using the appropriate string method.
+# 3. Print the converted lowercase string as the output.
