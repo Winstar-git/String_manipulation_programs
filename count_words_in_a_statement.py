f9ddc692553a8c9e2a4916af32bc03e8e3690171
Winstar <james.canoy87@gmail.com>
2025-03-22 02:25:28 +0800

    Implement word count

--- count_words_in_a_statement.py
+++ count_words_in_a_statement.py
@@ -6,6 +6,10 @@
 
 # Algorithm:
 # 1. Prompt the user to input a complete statement.
+statement = str(input("Enter statement: "))
 # 2. Split the input statement into words using whitespace as the delimiter.
+split_statement = statement.split()
 # 3. Count the number of words in the resulting list.
+count_statement = len(split_statement)
 # 4. Print the total word count.
+print(f"Output: {count_statement}")
