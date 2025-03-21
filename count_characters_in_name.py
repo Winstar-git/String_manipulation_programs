c1f8829e61ec85a56d2dbff568ae2a7329867433
Winstar <james.canoy87@gmail.com>
2025-03-22 02:31:49 +0800

    Algorithm for this program

---
+++ count_characters_in_name.py
@@ -0,0 +1,10 @@
+# Prog08: Create a program that ask the user to input their fullname. 
+# Print the number of characters in the input.
+# Example:
+# Input: Juan Dela Cruz
+# Output: 14
+
+# Algorithm:
+# 1. Prompt the user to input a complete statement.
+# 3. Count the number of character in the resulting list including spaces.
+# 4. Print the total word count.
