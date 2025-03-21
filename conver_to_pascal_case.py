42cd51071b5d0bfd25d73ad443b2d35aea208161
Winstar <james.canoy87@gmail.com>
2025-03-22 02:41:27 +0800

    Algorithm for this program

---
+++ conver_to_pascal_case.py
@@ -0,0 +1,11 @@
+# Prog09: Create a program that ask the user to input their fullname in incorrect casing. 
+# Print the input in pascal case.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: JuanDelaCruz
+
+# Algorithm:
+# 1. Prompt the user to input their full name in incorrect casing.
+# 2. Convert the input string to title case using the `title()` method.
+# 3. Remove all spaces from the title-cased string using the `replace()` method.
+# 4. Print the resulting PascalCase string.
