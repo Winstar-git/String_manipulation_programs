dc09d7213b5848598cac6507883b1b8d01283f16
Winstar <james.canoy87@gmail.com>
2025-03-22 01:43:34 +0800

    Add user input conversion to uppercase

--- convert_to_uppercase.py
+++ convert_to_uppercase.py
@@ -4,7 +4,9 @@
 # Input: Juan Dela Cruz
 # Output: JUAN DELA CRUZ
 
-# Algorithm:
-# 1. Prompt the user to input their full name.
+# 1. Prompt the user to input their fullname.
+fullname = input("Enter a fullname: ")
 # 2. Convert the input string to uppercase using the appropriate string method.
+uppercase_name = str(fullname.upper())
 # 3. Print the converted uppercase string as the output.
+print(f"Output: {uppercase_name}")
