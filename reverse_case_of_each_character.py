fed1963b9e250bd8ff4d1030884c37b6fdbc8492
Winstar <james.canoy87@gmail.com>
2025-03-22 02:03:23 +0800

    Algorithm for this program

---
+++ reverse_case_of_each_character.py
@@ -0,0 +1,9 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. 
+# Print each character of the input in reverse casing.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: JuaN deLA cRuz
+
+# 1. Prompt the user to input their fullname.
+# 2. Convert the input string and  use the swapcase() to reverse the case of each character.
+# 3. Print the reverse case string as the output.
