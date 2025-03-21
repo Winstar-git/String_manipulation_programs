cff5022294c5d2ce40f9c2892211bb218c8917b1
Winstar <james.canoy87@gmail.com>
2025-03-22 02:49:41 +0800

    Algorithm for this program

---
+++ convert_to_snakecase.py
@@ -0,0 +1,11 @@
+# Prog10: Create a program that ask the user to input their fullname in incorrect casing. 
+# Print the input in snake case.
+# Example:
+# Input: jUAn DEla CrUZ
+# Output: juan_dela_cruz
+
+# Algorithm:
+# 1. Prompt the user to input their full name in incorrect casing.
+# 2. Convert the input string to lower case using the  lower()` method.
+# 3. Remove all spaces from the lower-cased string using the `replace()` method.
+# 4. Print the resulting snake case string.
