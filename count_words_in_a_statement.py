701375bb672ec14fc5b0f7ed45993fdfbddef130
Winstar <james.canoy87@gmail.com>
2025-03-22 02:12:56 +0800

    Algorithm for this program

---
+++ count_words_in_a_statement.py
@@ -0,0 +1,11 @@
+# Prog07: Create a program that ask the user to input a complete statement. 
+# Print the number of words in the input.
+# Example:
+# Input: We will weather the weather whatever the weather whether we like it or not
+# Output: 14
+
+# Algorithm:
+# 1. Prompt the user to input a complete statement.
+# 2. Split the input statement into words using whitespace as the delimiter.
+# 3. Count the number of words in the resulting list.
+# 4. Print the total word count.
