162cdd916f991284ad55033ac7ef16a40c57e72a
Winstar <james.canoy87@gmail.com>
2025-03-22 02:53:28 +0800

    Add user input conversion to snake case

--- convert_to_snakecase.py
+++ convert_to_snakecase.py
@@ -6,6 +6,10 @@
 
 # Algorithm:
 # 1. Prompt the user to input their full name in incorrect casing.
+fullname = str(input("Enter a fullname: "))
 # 2. Convert the input string to lower case using the  lower()` method.
+name = fullname.lower()
 # 3. Remove all spaces from the lower-cased string using the `replace()` method.
+snake_case = name.replace(" ", "_")
 # 4. Print the resulting snake case string.
+print(f"Output: {snake_case}")
