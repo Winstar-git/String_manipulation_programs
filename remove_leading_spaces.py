da5b116a513b8190065f0980e37a15bb78100aa4
Winstar <james.canoy87@gmail.com>
2025-03-22 01:10:05 +0800

    Using Strip() to remove leading spaces

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -4,7 +4,9 @@
 # Input:         Juan Dela Cruz
 # Output: Juan Dela Cruz
 
-# Algorithm:
 # 1. Prompt the user to input their full name, which may include leading spaces.
+fullname = str(input("Enter your full name with spaces at the beginning: "))
 # 2. Use the `strip()` method to remove any leading and trailing spaces from the input.
+name = fullname.lstrip()
 # 3. Print the cleaned-up full name without the leading spaces.
+print(name)
