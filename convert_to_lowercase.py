06c044e44e7d18d65c34787e2f77ee055bf02d0a
Winstar <james.canoy87@gmail.com>
2025-03-22 01:48:22 +0800

    Add user input conversion to lowercase

--- convert_to_lowercase.py
+++ convert_to_lowercase.py
@@ -5,5 +5,8 @@
 # Output: juan dela cruz
 
 # 1. Prompt the user to input their fullname.
+fullname = input("Enter a fullname: ")
 # 2. Convert the input string to lowercase using the appropriate string method.
+lowercase_name = str(fullname.lower())
 # 3. Print the converted lowercase string as the output.
+print(f"Output: {lowercase_name}")
