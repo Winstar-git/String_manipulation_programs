d06926edc55a27c0d8be07fefb1cf398491e9e0e
Winstar <james.canoy87@gmail.com>
2025-03-22 02:36:15 +0800

    Implemented character count including spaces in fullname

--- count_characters_in_name.py
+++ count_characters_in_name.py
@@ -6,5 +6,8 @@
 
 # Algorithm:
 # 1. Prompt the user to input a complete statement.
-# 3. Count the number of character in the resulting list including spaces.
+fullname = str(input("Enter a fullname: "))
+# 3. Count the number of words in the resulting list including spaces.
+count_characters = len(fullname) 
 # 4. Print the total word count.
+print(f"Output: {count_characters}")
