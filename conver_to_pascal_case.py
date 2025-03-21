7cb85611ff378199f2d84144c993f2cc0f088f91
Winstar <james.canoy87@gmail.com>
2025-03-22 02:45:45 +0800

    Add user input conversion to pascal case

--- conver_to_pascal_case.py
+++ conver_to_pascal_case.py
@@ -6,6 +6,10 @@
 
 # Algorithm:
 # 1. Prompt the user to input their full name in incorrect casing.
+fullname = str(input("Enter a fullname: "))
 # 2. Convert the input string to title case using the `title()` method.
+name = fullname.title()
 # 3. Remove all spaces from the title-cased string using the `replace()` method.
+pascal_case = name.replace(" ","")
 # 4. Print the resulting PascalCase string.
+print(f"Output: {pascal_case}")
