15ac22366d6f5b3b55020071b61dcd3552c364b4
Winstar <james.canoy87@gmail.com>
2025-03-22 01:46:04 +0800

    Algorithm for this program

---
+++ convert_to_lowercase.py
@@ -0,0 +1,9 @@
+# Prog04: Create a program that ask the user to input their fullname. 
+# Print the input in all lower case.
+# Example:
+# Input: Juan Dela Cruz
+# Output: juan dela cruz
+
+# 1. Prompt the user to input their fullname.
+# 2. Convert the input string to lowercase using the appropriate string method.
+# 3. Print the converted lowercase string as the output.
